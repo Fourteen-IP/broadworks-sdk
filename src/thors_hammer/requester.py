21b7b8163213c690706f772e667f1347b35c184c
malkin0xb8 <michael.clarke@fourteenip.com>
2025-07-11 09:33:07 +0100

    Setup skeleton requester classes for asyn and sync

--- src/thors_hammer/requester.py
+++ src/thors_hammer/requester.py
@@ -1,4 +1,31 @@
 # this will be resposnsible for sending and receiving data from the API
+import httpx
+import asyncio
+
 
 class Requester:
-    pass
+    def __init__(self):
+        self.sync = SyncRequester()
+        self.async_ = AsyncRequester()
+
+
+class SyncRequester:
+    def __init__(self):
+        pass
+
+    def send_command_soap(self):
+        pass
+
+    def send_command_tcp(self):
+        pass
+
+
+class AsyncRequester:
+    def __init__(self):
+        pass
+
+    def send_command_soap(self):
+        pass
+
+    def send_command_tcp(self):
+        pass
