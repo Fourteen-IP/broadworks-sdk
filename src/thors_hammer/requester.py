53a2b3a3423e44670dafd924a05672035c90a6bb
Malkin0xb <42589044+malkin0xb8@users.noreply.github.com>
2025-07-17 15:41:12 +0100

    Th 68 add requester (#7)
    * Setup skeleton requester classes for asyn and sync
    
    * Implement all 4 classes and baseclass with SOAP and TCP commands
    
    Currently is just baseline and for testing
    
    Will need to implement passing over the auth headers
    
    * Add handling so that the socket remains open for authentication
    
    * Added connect and disconnect alongside automatic disconnection so that socket remains open for Broadoworks Auth
    
    * Add Logging
    
    ---------
    
    Co-authored-by: KiPageFault <157066925+KiPageFault@users.noreply.github.com>

--- src/thors_hammer/requester.py
+++ src/thors_hammer/requester.py
@@ -1,14 +1,247 @@
 # this will be resposnsible for sending and receiving data from the API
+import httpx
+import asyncio
+import socket
+import logging
+from typing import Any
+from .commands.base_command import BroadworksCommand
+from abc import ABC, abstractmethod
 
-class Requester:
-    pass
+class BaseRequester(ABC):
+    def __init__(self, logger: logging.Logger, host: str, port: int, timeout: int):
+        self.logger = logger
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.session_id = ""
+
+    @abstractmethod
+    def send_command(self, command: BroadworksCommand) -> Any:
+        pass
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        pass
+
+    def __del__(self):
+        self.disconnect()
+
+
+class SyncTCPRequester(BaseRequester):
+    def __init__(
+        self, logger: logging.Logger, host: str, port: int = 2208, timeout: int = 10
+    ):
+        super().__init__(host, port, timeout, logger)
+        self.sock = None
+
+    def connect(self):
+        if self.sock is None:
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.settimeout(self.timeout)
+                self.sock.connect((self.host, self.port))
+            except Exception as e:
+                self.logger.error(f"Failed to initiate socket on {self.__name__}: {e}")
+
+    def disconnect(self):
+        if self.sock:
+            try:
+                self.sock.close()
+            except Exception as e:
+                self.logger.warning(
+                    f"Exception: {e} was raised when attemping to close {self.__name__}, but was ignored."
+                )
+                pass
+            finally:
+                self.sock = None
+
+    def send_command(self, command: BroadworksCommand) -> Any:
+        try:
+            if self.sock is None:
+                self.connect()
+
+            self.sock.sendall(command)
+
+            response = b""
+            while True:
+                data = self.sock.recv(1024)
+                if not data:
+                    break
+                response += data
+
+            return response.decode("utf-8")
+        except Exception as e:
+            self.logger.error(f"Failed to send command over {self.__name__}: {e}")
+            return None
+
+    def __del__(self):
+        self.disconnect()
+
+
+class SyncSOAPRequester(BaseRequester):
+    def __init__(
+        self, logger: logging.Logger, host: str, port: int = 2208, timeout: int = 10
+    ):
+        super().__init__(host, port, timeout, logger)
+        self.client = None
+
+    def connect(self):
+        if self.client is None:
+            try:
+                self.client = httpx.Client(timeout=self.timeout)
+            except Exception as e:
+                self.logger.error(f"Failed to initiate client on {self.__name__}: {e}")
+
+    def disconnect(self):
+        if self.client:
+            try:
+                self.client.close()
+            except Exception as e:
+                self.logger.warning(
+                    f"Exception: {e} was raised when attemping to close {self.__name__}, but was ignored."
+                )
+                pass
+            finally:
+                self.client = None
+
+    def send_command(self, command: BroadworksCommand) -> Any:
+        try:
+            headers = {"Content-Type": "text/xml; charset=utf-8", "SOAPAction": ""}
+
+            response = self.client.post(self.url, headers=headers, data=command)
+
+            self.client.close()
+            return response.text
+        except Exception as e:
+            self.logger.error(f"Failed to send command over {self.__name__}: {e}")
+            return None
+
+    def __del__(self):
+        self.disconnect()
+
+
+class AsyncTCPRequester(BaseRequester):
+    def __init__(
+        self, logger: logging.Logger, host: str, port: int = 2208, timeout: int = 10
+    ):
+        super().__init__(host, port, timeout, logger)
+        self.reader = None
+        self.writer = None
+
+    async def connect(self):
+        if self.reader and self.writer is None:
+            try:
+                self.reader, self.writer = await asyncio.open_connection(
+                    host=self.host, port=self.port
+                )
+            except Exception as e:
+                self.logger.error(f"Failed to initiate socket on {self.__name__}: {e}")
+
+    async def disconnect(self):
+        if self.reader and self.writer:
+            try:
+                self.writer.close()
+                await self.writer.wait_closed()
+            except Exception as e:
+                self.logger.warning(
+                    f"Exception: {e} was raised when attemping to close {self.__name__}, but was ignored."
+                )
+                pass
+            finally:
+                self.writer = None
+                self.reader = None
+
+    async def send_command(self, command: BroadworksCommand) -> Any:
+        try:
+            self.writer.write(command)
+            await self.writer.drain()
+
+            response = await self.reader.readuntil(b"</BroadsoftDocument>")
+
+            return response.decode()
+        except Exception as e:
+            self.logger.error(f"Failed to send command over {self.__name__}: {e}")
+            return None
+
+    def __del__(self):
+        self.disconnect()
+
+
+class AsyncSOAPRequester(BaseRequester):
+    def __init__(
+        self, logger: logging.Logger, host: str, port: int = 2208, timeout: int = 10
+    ):
+        super().__init__(host, port, timeout, logger)
+        self.client = None
+
+    def connect(self):
+        if self.client is None:
+            try:
+                self.client = httpx.Client(timeout=self.timeout)
+            except Exception as e:
+                self.logger.error(f"Failed to initiate client on {self.__name__}: {e}")
+
+    def disconnect(self):
+        if self.client:
+            try:
+                self.client.close()
+            except Exception as e:
+                self.logger.warning(
+                    f"Exception: {e} was raised when attemping to close {self.__name__}, but was ignored."
+                )
+                pass
+            finally:
+                self.client = None
+
+    async def send_command(self, command: BroadworksCommand) -> Any:
+        try:
+            headers = {"Content-Type": "text/xml; charset=utf-8", "SOAPAction": ""}
+
+            response_cor = await self.client.post(
+                self.url, headers=headers, data=command
+            )
+
+            response = await asyncio.gather(response_cor)
+
+            await self.client.aclose()
+
+            return response.text
+        except Exception as e:
+            self.logger.error(f"Failed to send command over {self.__name__}: {e}")
+            return None
+
+    def __del__(self):
+        self.disconnect()
+
 
 def create_requester(
-    conn_type,
-    async_mode,
-    host,
-    timeout,
-    logger,
-    session_id
-):
-    return 
+    logger: logging.Logger,
+    sessionid_id: str,
+    host: str,
+    port: int,
+    connection_type: str = "SOAP",
+    async_: bool = True,
+    timeout: int = 10,
+) -> BaseRequester:
+    if connection_type == "SOAP":
+        if async_:
+            return AsyncSOAPRequester(
+                host=host, port=port, timeout=timeout, logger=logger
+            )
+        else:
+            return SyncSOAPRequester(
+                host=host, port=port, timeout=timeout, logger=logger
+            )
+    elif connection_type == "TCP":
+        if async_:
+            return AsyncTCPRequester(
+                host=host, port=port, timeout=timeout, logger=logger
+            )
+        else:
+            return SyncTCPRequester(
+                host=host, port=port, timeout=timeout, logger=logger
+            )
